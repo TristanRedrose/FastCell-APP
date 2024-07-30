3bb3e20c21f849a21bc86e425c1deaa8caa72706
Ivan <TristanRedrose@hotmail.com>
2024-07-30 19:53:26 +0200

    add contact form to contact page, change contact page css, change store name to mobitronic

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,12 +1,58 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
+
 export class ContactFormComponent {
 
+
+  contactForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(500)]),
+    consent: new FormControl(false, Validators.requiredTrue),
+  })
+
+  onSubmit():void {
+    if (!this.consent.value) {
+      return
+    }
+
+    let contactFormInfo = {
+      firstName: this.firstName.value,
+      lastName: this.lastName.value,
+      email: this.email.value,
+      message: this.message.value,
+    }
+
+    console.log(contactFormInfo);
+  }
+
+  get firstName(): FormControl<string | null> {
+    return this.contactForm.controls.firstName;
+  }
+
+  get lastName(): FormControl<string | null> {
+    return this.contactForm.controls.lastName;
+  }
+
+  get email(): FormControl<string | null> {
+    return this.contactForm.controls.email;
+  }
+
+  get message(): FormControl<string | null> {
+    return this.contactForm.controls.message;
+  }
+
+  get consent(): FormControl<boolean | null> {
+    return this.contactForm.controls.consent;
+  }
 }
